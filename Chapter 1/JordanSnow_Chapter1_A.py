cc59e099ee1e694510df8e34a04388ab1b268949
Joral <snowjo7@yahoo.com>
2024-05-13 23:25:50 -0400

    Commit Changes

--- Chapter 1/JordanSnow_Chapter1_A.py
+++ Chapter 1/JordanSnow_Chapter1_A.py
@@ -2,14 +2,13 @@
 # Each customer can only purchase up to 4 tickets. The amount of tickets available will print out after each purchase.
 # Finally, after all tickets are sold, the number of customers will be displayed and a goodbye message will print.
 
-
-# Create Variables
-tickets = 20
-buyers = 0
-sold = 0
-
 # Define main
 def main():
+
+    # Create Variables
+    tickets = 20
+    buyers = 0
+    sold = 0
 
     # Make tickets and buyers global to avoid errors
     global tickets
