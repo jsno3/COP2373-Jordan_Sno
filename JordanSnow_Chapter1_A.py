fd44dc321ba5e2374b332cfa8f14757b9872cfe6
Joral <snowjo7@yahoo.com>
2024-05-13 21:39:41 -0400

    Commit Test

--- JordanSnow_Chapter1_A.py
+++ JordanSnow_Chapter1_A.py
@@ -1,13 +1,15 @@
 # brief description of what the code is doing
 tickets = 20
+buyers = 0
 sold = 0
 
-
 def main():
+    global tickets
     if tickets <= 20:
-        sold = input('How many tickets would you like to purchase? ')
-        tickets -= int(sold)
-        print(format('There are {tickets} tickets remaining to be sold.'))
+        sold = input('How many tickets would you like to purchase? (up to four can be purchased per customer) ')
+        if  0 < int(sold) <= 4:
+            tickets -= int(sold)
+            print(f'There are {tickets} tickets remaining to be sold.')
         if tickets > 0:
             main()
         else:
