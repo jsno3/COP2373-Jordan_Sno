700dd242f72b2ab1f0a15fb168f56c43f63af377
Joral <snowjo7@yahoo.com>
2024-05-13 22:08:57 -0400

    Commit Assignment

--- JordanSnow_Chapter1_A.py
+++ JordanSnow_Chapter1_A.py
@@ -1,4 +1,7 @@
-# Description
+# This program is intended to pre-sell 20 tickets to prospective customers.
+# Each customer can only purchase up to 4 tickets. The amount of tickets available will print out after each purchase.
+# Finally, after all tickets are sold, the number of customers will be displayed and a goodbye message will print.
+
 
 # Create Variables
 tickets = 20
@@ -8,11 +11,11 @@ sold = 0
 # Define main
 def main():
 
-    # Make tickets global to avoid errors
+    # Make tickets and buyers global to avoid errors
     global tickets
 
     # Create if statement to sell tickets if there is any available
-    if  tickets <= 20:
+    if tickets <= 20:
         sold = input('How many tickets would you like to purchase? (up to four can be purchased per customer) ')
 
         # Create if statement to check the amount of tickets the buyer is attempting to purchase
