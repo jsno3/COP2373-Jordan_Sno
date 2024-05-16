787b79f48822500d66b7867a8ae67455a283e159
Joral <snowjo7@yahoo.com>
2024-05-16 17:59:18 -0400

    Commit Changes

---
+++ Chapter 1/JordanSnow_Chapter1_1B.py
@@ -0,0 +1,7 @@
+
+def main():
+    #f = open("8ball_responses.txt", "x")
+    responses = ["Yes, of course!", "Without a doubt, yes.", "You can count on it.", "For sure!", "Ask me later!", "I'm not sure.", "I can't tell you right now.'", "I'll tell you after my nap.", "No way!", "I don't think so.", "Without a doubt, no.", "The answer is clearly NO!"]
+    print(responses)
+
+main()
