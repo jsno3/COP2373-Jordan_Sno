91d27f7172004a45ee79313477b701e50203945a
Joral <snowjo7@yahoo.com>
2024-05-24 14:38:04 -0400

    Commit Changes

--- Chapter 2/JordanSnow_Chapter2_2A.py
+++ Chapter 2/JordanSnow_Chapter2_2A.py
@@ -1,4 +1,10 @@
 #
 
 spam_list = ['100% free', 'Prize', 'Act now', 'Winner', 'Winning', 'You are a winner', 'Click here', 'Click below', 'Important information regarding', 'Congratulations', 'No catch', 'Notspam', 'Offer', 'Discount', 'Bargain', 'Amazing', 'Free', 'Deal', 'Click', 'Cheap', 'Earn', 'Cost', 'Best price', 'Credit', 'Cash', 'Apply', 'Billion', 'Debt', 'Bankruptcy', 'Immediate delivery']
-print(len(spam_list))
+sorted_spam_list = sorted(spam_list)
+cap_spam = []
+for word in sorted_spam_list:
+    cap_spam.append(word.title())
+print(sorted_spam_list)
+print(cap_spam)
+
