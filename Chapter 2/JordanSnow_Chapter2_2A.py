4dc825528f65fa6eb1820f7b0f89835d20b1c540
Joral <snowjo7@yahoo.com>
2024-05-24 14:29:46 -0400

    Commit Changes

--- Chapter 2/JordanSnow_Chapter2_2A.py
+++ Chapter 2/JordanSnow_Chapter2_2A.py
@@ -1,1 +1,4 @@
+#
 
+spam_list = ['100% free', 'Prize', 'Act now', 'Winner', 'Winning', 'You are a winner', 'Click here', 'Click below', 'Important information regarding', 'Congratulations', 'No catch', 'Notspam', 'Offer', 'Discount', 'Bargain', 'Amazing', 'Free', 'Deal', 'Click', 'Cheap', 'Earn', 'Cost', 'Best price', 'Credit', 'Cash', 'Apply', 'Billion', 'Debt', 'Bankruptcy', 'Immediate delivery']
+print(len(spam_list))
