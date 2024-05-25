632d63f1619297eac9dc33d2fc63cac6ae48dde7
Joral <snowjo7@yahoo.com>
2024-05-24 22:28:43 -0400

    Commit Changes

--- Chapter 2/JordanSnow_Chapter2_2A.py
+++ Chapter 2/JordanSnow_Chapter2_2A.py
@@ -1,7 +1,7 @@
 #
 def spam():
     global cap_spam
-    spam_list = ['100% free', 'Prize', 'Act now', 'Winner', 'Winning', 'You are a winner', 'Click here', 'Click below', 'Important information regarding', 'Congratulations', 'No catch', 'Notspam', 'Offer', 'Discount', 'Bargain', 'Amazing', 'Free', 'Deal', 'Click', 'Cheap', 'Earn', 'Cost', 'Best price', 'Credit', 'Cash', 'Apply', 'Gift', 'Debt', 'Receive', 'You have won']
+    spam_list = ['100% free', 'Prize', 'Act now', 'Winner', 'Winning', 'You are a winner', 'Click here', 'Click below', 'Important information regarding', 'Congratulations', 'No catch', 'Not spam', 'Offer', 'Discount', 'Bargain', 'Amazing', 'Free', 'Deal', 'Click', 'Cheap', 'Earn', 'Cost', 'Best price', 'Credit', 'Cash', 'Apply', 'Gift', 'Debt', 'Receive', 'You have won']
     sorted_spam_list = sorted(spam_list)
     cap_spam = []
     for word in sorted_spam_list:
@@ -15,41 +15,57 @@ spam()
 
 
 def main():
-    global spam_likelihood
+    global percent_spam
     message_length = 0
     spam_score = 0
     spam_words_detected = []
 
-    message = input('Please enter the email message you want scanned: ')
+    message = input('Please enter the email/message you want scanned: ')
+    if len(message) == 0:
+        print('Please enter at least one word.')
+        main()
+    else:
+        pass
     cap_message = message.title()
     message_length = len(message.split())
     for i in range(len(cap_spam)):
         if cap_spam[i] in cap_message:
-            print(cap_spam[i])
             spam_words_detected.append(cap_spam[i])
             spam_score += 1
 
     percent_spam = round((spam_score / message_length) * 100)
-    spam_likelihood = (f'Out of the {message_length} words provided, {percent_spam}% of the words/phrases have been marked as common contents of spam emails.')
+    spam_likelihood = f'Out of the {message_length} words provided, {percent_spam}% of the words/phrases have been marked as common contents of spam emails.'
 
 
-
-    print(spam_score)
-    print(spam_words_detected)
-    print(message)
-    print(message_length)
+    print(f'\nYour provided email/message was: "{message}"')
+    print(f'Your spam score for this email/message was: {spam_score}/{message_length}')
     print(spam_likelihood)
+    print(f'Based on this data, {sec()}')
+    print('The following words/phrases within your email/message were detected as potential spam:')
+    for word in spam_words_detected:
+        print(f'"{word}"')
 
-main()
+    run = input('Would you like to enter another email/message? (y/n) ')
+    if run == 'y':
+        main()
+    elif run == 'Y':
+        main()
+    else:
+        print('\nThank you for using our spam detecting program.\nGoodbye!')
+        exit()
+
 
 def sec():
-    if spam_likelihood <= 20:
-        return "The provided email is very unlikely to be spam."
-    elif spam_likelihood <= 40:
-        return "The provided email is not likely to be spam."
-    elif spam_likelihood <= 60:
-        return "The provided email is probably spam."
-    elif spam_likelihood <= 80:
-        return "The provided email is likely to be spam."
-    elif spam_likelihood <= 100:
-        return "The provided email is very likely to be spam."
+    if percent_spam <= 20:
+        return "the provided email is very unlikely to be spam."
+    elif percent_spam <= 40:
+        return "the provided email is not likely to be spam."
+    elif percent_spam <= 60:
+        return "the provided email is probably spam."
+    elif percent_spam <= 80:
+        return "the provided email is likely to be spam."
+    elif percent_spam <= 100:
+        return "the provided email is very likely to be spam."
+
+
+main()
