1d87d4c53698fbf8e9faf1bea94d0cbd1f18b385
Joral <snowjo7@yahoo.com>
2024-06-23 19:17:21 -0400

    Commit Changes

--- Chapter 7/Programming Exercise 7A.py
+++ Chapter 7/Programming Exercise 7A.py
@@ -1,1 +1,15 @@
 import re
+
+count = 0
+
+pattern = r'(?<=(^|(. )))[A-Z\d].*?(.{3}|[.!?])(?=( [A-Z\d])|$)'
+
+entry = input("Type in any number of sentences, including sentences which begin with numbers.")
+
+number_of_sentences = re.findall(pattern, entry)
+
+for sentence in number_of_sentences:
+    print(sentence)
+    count += 1
+
+print(len(number_of_sentences))
