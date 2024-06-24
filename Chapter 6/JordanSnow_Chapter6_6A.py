243e8c144af89277b9fed9d47df5e4fb4c6e46f7
Joral <snowjo7@yahoo.com>
2024-06-23 20:39:35 -0400

    Commit Changes

--- Chapter 6/JordanSnow_Chapter6_6A.py
+++ Chapter 6/JordanSnow_Chapter6_6A.py
@@ -6,3 +6,22 @@ def valid_phone_number(phone_number):
 
     return bool(re.match(pattern, phone_number))
 
+valid_numbers = ['123-456-7890', '(123) 456-7890', '123 456 7890']
+
+invalid_numbers = ['1234567890', '123-456-789', '123-456-789a']
+
+for number in valid_numbers:
+    print(f'{number} is a valid phone number: {valid_phone_number(number)}')
+
+for number in valid_numbers:
+    print(f'{number} is a valid phone number: {valid_phone_number(number)}')
+
+def valid_social_security_number(ssn):
+    pattern = '^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$'
+    match = re.match(pattern, ssn)
+    return bool(match)
+
+valid_ssn = ['123 45 6789', '987 65 4321', '123-45-6789']
+
+invalid_ssn = ['000 12 3456', '123 45 0000']
+
