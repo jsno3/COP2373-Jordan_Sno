bbaf88a19adb0b70954ff05c3697fe0f22310c78
Joral <snowjo7@yahoo.com>
2024-06-29 23:41:18 -0400

    Commit Changes

--- Chapter 8/Programming Exercise CSV.py
+++ Chapter 8/Programming Exercise CSV.py
@@ -1,18 +1,36 @@
 # Format
-# First name, Last name, grades (integers)
+# first_name,last_name,exam_1,exam_2,exam_3 (integers)
 
 import csv
 
-with open('grades.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
 
-# entries_amount = input('How many students are being entered? ')
+def main():
 
-    # for i in entries_amount:
-    with open('new_grades.csv', 'w') as new_file:
-        csv_writer = csv.writer(new_file)
+    entries_amount = input('How many students are being entered? ')
 
-    # next(csv_reader)
+    with open('grades.csv', 'w', newline='') as new_file:
 
-        for line in csv_reader:
-            csv_writer.writerow(line)
+        fieldnames = ['first_name', 'last_name', 'exam_1', 'exam_2', 'exam_3']
+
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+
+        csv_writer.writeheader()
+
+        for i in range(int(entries_amount)):
+
+            first = input("Enter Student's First Name: ")
+
+            last = input("Enter Student's Last Name: ")
+
+            exam_1 = int(input("Enter Student's Grade for Exam 1: "))
+
+            exam_2 = int(input("Enter Student's Grade for Exam 2: "))
+
+            exam_3 = int(input("Enter Student's Grade for Exam 3: "))
+
+            d = {'first_name': first, 'last_name': last, 'exam_1': exam_1, 'exam_2': exam_2, 'exam_3': exam_3}
+
+            csv_writer.writerow(d)
+
+
+main()
