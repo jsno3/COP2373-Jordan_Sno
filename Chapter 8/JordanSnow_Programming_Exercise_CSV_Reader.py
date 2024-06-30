0b7d28906216f4f680b9159aca9abc0712679efc
Joral <snowjo7@yahoo.com>
2024-06-30 14:56:32 -0400

    Commit Changes

--- Chapter 8/JordanSnow_Programming_Exercise_CSV_Reader.py
+++ Chapter 8/JordanSnow_Programming_Exercise_CSV_Reader.py
@@ -1,15 +1,21 @@
+# The purpose of this program is to read the created csv file: 'grades.csv' and display it in a formatted DataFrame
+
+# Import package 'pandas' as 'pd' for further use in the program
 import pandas as pd
 
 
+# Define main
 def main():
 
   # Open the CSV file in read mode with 'with' keyword
   with open('grades.csv', 'r'):
 
+    # Set variable 'df' equal to the result of reading the csv file into a DataFrame
     df = pd.read_csv('grades.csv')
 
     # Print DataFrame with better formatting
     print(df.to_string(index=False))
 
 
+# Call main
 main()
--- Chapter 8/JordanSnow_Programming_Exercise_CSV_Writer.py
+++ Chapter 8/JordanSnow_Programming_Exercise_CSV_Writer.py
@@ -1,36 +1,54 @@
-# Format
-# first_name,last_name,exam_1,exam_2,exam_3 (integers)
+""" The purpose of this program is to allow an instructor to input the number of students
+they wish to enter into a csv file, and then the program will iterate through a loop to enter
+the students first name, last name, and their grades for three separate exams.
+"""
 
+# Import csv to write 'grades.csv' later in program
 import csv
 
 
+# Define main
 def main():
 
+    # Set variable 'entries_amount' equal to the input from the user dictating how many students they wish to enter
     entries_amount = input('How many students are being entered? ')
 
+    # Open or create 'grades.csv' in write mode, set newline='', as new_file
     with open('grades.csv', 'w', newline='') as new_file:
 
+        # Define variable 'fieldnames' to be used in when writing the csv file
         fieldnames = ['First Name', 'Last Name', 'Exam 1', 'Exam 2', 'Exam 3']
 
+        # Define variable 'csv_writer' and set it equal to 'csv.DictWriter(new_file, fieldnames=fieldnames)'
         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
 
+        # Write headers (First Name, Last Name, Exam 1, Exam 2, Exam 3) for better organization
         csv_writer.writeheader()
 
+        # For loop, for 'i' in the range of 'entries_amount' (the number of students to be entered):
         for i in range(int(entries_amount)):
 
+            # Set variable 'first' equal to the input for string: 'Enter Student's First Name: '
             first = input("Enter Student's First Name: ")
 
+            # Set variable 'last' equal to the input for string: 'Enter Student's Last Name: '
             last = input("Enter Student's Last Name: ")
 
+            # Set variable 'exam_1' equal to the input for string: 'Enter Student's Grade for Exam 1: '
             exam_1 = int(input("Enter Student's Grade for Exam 1: "))
 
+            # Set variable 'exam_2' equal to the input for string: 'Enter Student's Grade for Exam 2: '
             exam_2 = int(input("Enter Student's Grade for Exam 2: "))
 
+            # Set variable 'exam_3' equal to the input for string: 'Enter Student's Grade for Exam 3: '
             exam_3 = int(input("Enter Student's Grade for Exam 3: "))
 
+            # Create variable 'd' and set it equal to a dictionary that assigns the declared fields with the inputted related data
             d = {'First Name': first, 'Last Name': last, 'Exam 1': exam_1, 'Exam 2': exam_2, 'Exam 3': exam_3}
 
+            # Write a new row to 'grades.csv' passing in the dictionary 'd'
             csv_writer.writerow(d)
 
 
+# Call main
 main()
