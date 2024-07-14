61a0ecf249c4038ccec5d4d9ba29cdb630755d0d
Joral <snowjo7@yahoo.com>
2024-07-13 22:57:53 -0400

    Commit Changes

--- Chapter 10/JordanSnow_Chapter10_10A.py
+++ Chapter 10/JordanSnow_Chapter10_10A.py
@@ -7,20 +7,127 @@ class Money(Decimal):
 
         return super(Money, cls).__new__(cls, v)
 
-    def __init__(self, v, units='USD'):
+    def __init__(self, v: float|int, units='USD'):
 
         self.units = units
+        self.v = v
 
-    def addition(self):
+    def __str__(self):
+        return f'{self.v:.2f} {self.units}'
+
+    def __add__(self, value):
+        self.v += value
+        return self
+
+    def __sub__(self, value):
+        self.v -= value
+        return self
+
+    def __mul__(self, value):
+        self.v *= value
+        return self
+
+    def __truediv__(self, value):
+        self.v /= value
+        return self
+
+def main():
+
+    # Declare variable 'test' as a global variable
+    global test
+
+    # Print statement greeting the user
+    print('Hello! Welcome to the Money Moves Program!')
+
+    # Set variable 'name' equal to a string from the user declaring their name
+    v = float(input('How much money do you have? '))
+
+    # Set variable 'account' equal to a string from the user declaring their account number
+    # (up to 9 digits parameter just for flair, not implemented in any way)
+    # units = input('What units do you want to use our program in? ')
+
+    # Set 'test' equal to class 'Money' with the above variables passed in
+    test = Money(v=v, units='USD')
+
+    # Print 'test' to user to display 'general info' about their account
+    print('You are starting with', test)
 
 
-    def subtraction(self):
+main()
 
 
-    def multiplication(self):
+def test_func():
+
+    # Print multiple strings that outline the services offered
+    print('Please Make a Selection for Our Services'
+          '\n1 - Addition '
+          '\n2 - Subtraction '
+          '\n3 - Multiplication '
+          '\n4 - Division'
+          '\n5 - View Amount \n6 - Exit ')
+
+    # Set variable 'selection' equal to the inputted value from the user (to make a selection from the given options)
+    selection = input('Enter the corresponding number to the services you require: ')
+
+    # If/elif/else statement, if 'selection' is '1' call the add method
+    if selection == '1':
+
+        # Print the 'add' method
+        x = float(input("Enter the amount you would like to add: "))
+        print(test + x)
+
+    # Elif statement, if 'selection' is '2' call the sub method
+    elif selection == '2':
+
+        # Print the 'sub' method
+        print(test.show_interest_prediction())
+
+    # Elif statement, if 'selection' is '3' call the 'mul' method
+    elif selection == '3':
+
+        # Print the 'mul' method
+        print(test.withdraw())
+
+    # Elif statement, if 'selection' is '4' call the 'truediv' method
+    elif selection == '4':
+
+        # Print the 'truediv' method
+        print(test.deposit())
+
+    # Elif statement, if 'selection' is '5' print 'test' and show the general info for the account
+    elif selection == '5':
+
+        # Print 'test'
+        print(test)
+
+    # Elif statement, if 'selection' is '6' exit the program
+    elif selection == '6':
+
+        # Exit the program
+        exit()
+
+    # Else statement, print 'That is not a valid selection and restart the test function
+    else:
+
+        # Print 'That is not a valid selection'
+        print('That is not a valid selection')
+
+        # Call 'test_func'
+        test_func()
+    # Call 'test_func'
+    test_func()
 
 
+# Call 'test_func'
+test_func()
 
+m = Money(1000.04, 'USD')
+print(m)
 
-m = Money('0.11', 'USD')
-print(m, m.units)
+print(m + 1)
+
+print(m - 1)
+
+print(m * 2.5)
+
+print(m / 2)
