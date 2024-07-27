c7f83d9b5cb1610ba7b0de26b9a659cd49fab202
Joral <snowjo7@yahoo.com>
2024-07-27 01:43:34 -0400

    Commit Changes

--- Chapter 12/JordanSnow_Chapter12_12A.py
+++ Chapter 12/JordanSnow_Chapter12_12A.py
@@ -1,14 +1,72 @@
 
-# import csv
+# from numpy import genfromtxt
 
-from numpy import genfromtxt
+import numpy as np
 
 def main():
 
-    array = genfromtxt('grades.csv', delimiter=',', dtype=None)
+    array = np.genfromtxt('grades.csv', delimiter=',', dtype = str)
 
     print(array)
 
-    exit()
+
+
+    print('Here are the first three lines of the array: \n', array[:3])
+
+    column_1 = array[1:, 2]
+    print(column_1)
+
+    column_2 = array[1:, 3]
+    print(column_2)
+
+    column_3 = array[1:, 4]
+    print(column_3)
+
+    column_1_sum = 0
+
+    for i in range(len(column_1)):
+
+        column_1_sum += (int(column_1[i]))
+
+        print(column_1[i])
+
+    column_1_stats = np.mean(column_1)
+    print(column_1_stats)
+
+    # Juniper = array[1]
+    #
+    # print(Juniper)
+    #
+    # Mary = array[2]
+    #
+    # print(Mary)
+    #
+    # Chester = array[3]
+    # print(Chester)
+    #
+    # Dan = array[4]
+    # print(Dan)
+    #
+    # Leonard = array[5]
+    # print(Leonard)
+    #
+    # Lydia = array[6]
+    # print(Lydia)
+    #
+    # Victoria = array[7]
+    # print(Victoria)
+    #
+    # Michael = array[8]
+    # print(Michael)
+    #
+    # Lily = array[9]
+    # print(Lily)
+    #
+    # Carter = array[10]
+    # print(Carter)
+
+
+
+    # exit()
 
 main()
